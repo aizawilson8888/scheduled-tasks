9e37d26fb1af08d9a9eb784d8e3a2c19a8cefe74
Aiza Wilson <aizawilson7388@gmail.com>
2026-03-05 15:22:56 -0500

    Rename environment variable for API key

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from twilio.rest import Client
 
 OWM_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
-api_key = os.environ.get("OWM_API_KEY")
+api_key = os.environ.get("API_KEY")
 account_sid = os.environ.get("ACC_SID")
 auth_token = os.environ.get("AUTH_TOKEN")
 
